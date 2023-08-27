d734717e5404c7877550f7dd1d4675e6c4398560
rusirugunaratne <info.rusiru@gmail.com>
2023-08-28 00:01:51 +0530

    conflict change

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         </Route>
         <Route path="/services" element={<Services />}>
         </Route>
-        <Route path="/" element={<AdminLayout />}>
+        <Route path="/" element={<Home />}>
         </Route>
       </Routes>
     </Router>
